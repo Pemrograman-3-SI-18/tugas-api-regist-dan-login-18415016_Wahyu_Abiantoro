45577dc433cad8c32be6f248d255a94db087b41d
wahyuabi <wahyu.18415016@student.ubl.ac.id>
2020-06-04 23:48:59 +0700

    tugas api regist dan login

---
+++ ApiTokoBuku/config/response.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    commonError:{
+        error: true,
+        msg : 'Terjadi Keglahan Pada Server'
+
+    },
+    commonErrorMsg: (msg) => {
+        return {
+            error : true,
+            msg : msg
+    }
+    },
+    commonSuccess: {
+        error: false,
+        msg : 'Berhasil Memuat Permintaan'
+    },
+    commonSuccessMsg: (msg) => {
+        return {
+            error: false,
+            msg : msg
+        }
+    },
+    commonResult: (data) => {
+        return {
+            error : false,
+            msg : 'Berhasil Memuat Data',
+            data : data
+        }
+    }
+};
